03c1adf0463a9b287c826c4972488b206d078b5f
Ben Dayan <ben.dayan@ge.com>
2023-12-09 09:31:12 +0200

    day01 - part 1 working

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,5 +1,14 @@
 use common::read_lines;
 
 fn main() {
-    println!("Hello, world!");
+    let lines = read_lines("day01/input").unwrap();
+    let mut sum = 0;
+    for line in lines {
+        let digits : Vec<_> = line.unwrap().chars().filter_map(|c| c.to_digit(10)).collect();
+        println!("{:?}", digits);
+        sum += digits[0] * 10;
+        sum += digits[..].last().unwrap();
+        println!("{}", sum);
+    }
+    // for each line in lines, split digits
 }
