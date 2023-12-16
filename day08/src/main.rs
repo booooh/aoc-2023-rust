fa96c9d00c8d879483871e5b22cde4f33f9a757f
Ben Dayan <ben.dayan@ge.com>
2023-12-16 18:15:23 +0200

    day 07 - part1

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,3 +1,60 @@
+use regex::Regex;
+use std::{collections::HashMap, fs};
+type Instructions = HashMap<String, [String; 2]>;
+
+#[derive(Debug)]
+struct Network {
+    step_list: Vec<usize>,
+    instructions: Instructions,
+}
+
+impl From<&str> for Network {
+    fn from(value: &str) -> Self {
+        let mut lines = value.split("\n").filter(|l| !l.is_empty());
+        let steps = lines.next().unwrap();
+        let step_list = steps
+            .chars()
+            .map(|c| match c {
+                'L' => 0,
+                'R' => 1,
+                _ => panic!("This should not happen"),
+            })
+            .collect();
+        let re = Regex::new(r"(?<from>[^ ]+) = \((?<left>.*), (?<right>.*)\)").unwrap();
+        let instructions: Instructions = lines
+            .filter(|line| !line.is_empty())
+            .map(|line| re.captures(line).unwrap())
+            .map(|cap| {
+                (
+                    cap["from"].into(),
+                    [cap["left"].into(), cap["right"].into()],
+                )
+            })
+            .collect();
+
+        Network {
+            step_list,
+            instructions,
+        }
+    }
+}
+
+fn part1() {
+    let lines = fs::read_to_string("day08/input").unwrap();
+    let network: Network = lines[..].into();
+    let mut curr_node = "AAA".to_string();
+
+    let mut step_list = network.step_list.iter().cycle();
+    let mut num_steps = 0;
+    while curr_node != "ZZZ" {
+        let step = step_list.next().unwrap();
+        curr_node = network.instructions[&curr_node][*step].clone();
+        num_steps += 1;
+        println!("went to node {}", curr_node);
+    }
+    println!("num steps: {}", num_steps);
+}
+
 fn main() {
-    println!("Hello, world!");
+    part1();
 }
